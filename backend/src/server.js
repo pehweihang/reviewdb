d47871b855889dfa6c06111e356c0c8755d8af0b
pehweihang <pehweihang@live.com>
2021-08-08 18:30:02 +0800

    added mongodb connection and router

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const app = express()
+const router = express.Router()
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true, 
+    user: process.env.MONGO_USER, 
+    pass: process.env.MONGO_PASSWORD
+})
 
 const PORT = process.env.PORT ? process.env.PORT : 3000;
 
 
 
-app.get('/', (request, response) =>{
-    response.send('<h1>Hello World!</h1>')
-})
-
+//Routes
+app.use(express.json())
+app.use('/',require('./routes/index'));
+app.use('/users',require('./routes/users'));
 
 app.listen(PORT, () => { 
 console.log(`Server running on port ${PORT}`);
