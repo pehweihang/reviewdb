fe2dab77d49770cf3c7064009693723f8d5d5ba2
pehweihang <pehweihang@live.com>
2021-08-08 18:28:29 +0800

    added name to user model

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -14,6 +14,11 @@ let UserSchema = new Schema({
         required: true,
     },
 
+    name: {
+        type: String,
+        required: true,
+    },
+
     email: {
         type: String,
         trim: true,
