6965ca3d4a538ef069610aba3d3f3b36fb851694
pehweihang <pehweihang@live.com>
2021-08-10 03:01:51 +0800

    added user auth

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const auth = require("../middleware/auth")
+const User = require('../models/user')
 
 
 router.get('/', (req, res)=>{
     res.render('welcome')
+})
+
+router.post("/welcome", auth, async (req, res)=>{
+    const user_id = res.locals.user_id
+    const user = await User.findOne({_id:user_id}) 
+    return res.status(200).json("Welcome "+user.name);
 })
 
 module.exports = router;
