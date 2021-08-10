25fe29621194bb6e5a6be7d7cec491cc93b3ed8f
Ryan Taw <ryan.taw96@gmail.com>
2021-08-10 22:48:13 +0800

    login page

--- frontend/pages/Login.js
+++ frontend/pages/Login.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
+import React, {useState} from 'react';
+import { 
+  Avatar, Button, Checkbox, CssBaseline, FormControlLabel, Paper, 
+  Grid, Typography, TextField, InputAdornment, IconButton, Link,
+} from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
+
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+const axios = require('axios');
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '100vh',
   },
   image: {
+    width:'100%',
+    height:'100%',
+    opacity: 0.4,
     backgroundImage: "url(/assets/top-manhwa.jpg)",
     backgroundColor:
       theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
     backgroundSize: 'cover',
     backgroundPosition: 'center',
+    justifyContent:'center',
+    alignItems:'center',
   },
   paper: {
     margin: theme.spacing(8, 4),
@@ -44,42 +47,98 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignInSide() {
   const classes = useStyles();
+  const [content,setContent] = useState({ 
+    username: '',
+    email: '',
+    name: '',
+    password: '',
+    password2: '',
+  });
+  const setEmail = (e) => {
+    setContent({
+      ...content,email: e.target.value,
+    })
+  }
+  const setPassword = (e) => {
+    setContent({
+      ...content,password: e.target.value,
+    })
+  }
 
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
+  const handleMouseDownPassword = () => setShowPassword(!showPassword);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(content);
+    axios.post('http://localhost:8080'+'/users/login',content
+    )
+    .then(response => {
+      console.log(response);
+    })
+  }
+  
   return (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={false} sm={4} md={7} style={{alignItems:'center'}}>
+      <div className={classes.image}/>
+      <div style={{width:'50%',alignSelf:'center',position:'absolute',top:'40%',color:'white',}}>
+      <Typography variant='h3' >Review your favourite webtoons now! On ReviewDB</Typography>
+      </div>
+      </Grid>
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in
+            Login
           </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
+          <form className={classes.form} onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={12}>
+              <TextField
+                onChange={setEmail}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+              <TextField
+                onChange={setPassword}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type={showPassword ? "text" : "password"}
+                id="password"
+                autoComplete="current-password"
+                InputProps={{ // <-- This is where the toggle button is added.
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  )
+                }}
+              />
+              </Grid>
             <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
@@ -104,6 +163,7 @@ export default function SignInSide() {
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
+            </Grid>
             </Grid>
           </form>
         </div>
