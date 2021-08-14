7f8fbb9d822b133ef2b6ea016b1a772209348bf2
weihang <pehweihang@live.com>
2021-08-15 05:32:03 +0800

    graphql and ts

---
+++ server/src/entity/User.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    unique: true,
+  })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  name: string;
+
+  @Column({
+    type: "text",
+    nullable: true,
+  })
+  group: number | null;
+}
